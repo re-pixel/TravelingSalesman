700cffcbe8483233644c98be0d936f979e9155a8
IgorAmi52 <igorami44@gmail.com>
2025-06-16 15:45:34 +0200

    Optimality metric added & better printing

--- TSM.py
+++ TSM.py
@@ -1,10 +1,12 @@
 import random
 import math
+import time
+
 
 class TSM:
-    def __init__(self, data, population_size=100, seed=None, elite_size_factor=0.1, mutation_rate=0.1, 
-                max_generations=1000, crossover_rate=0.7, mating_pool_size_factor=0.5):
-        
+    def __init__(self, data, population_size=100, seed=None, elite_size_factor=0.1, mutation_rate=0.1,
+                 max_generations=1000, crossover_rate=0.7, mating_pool_size_factor=0.5):
+
         self.data = data
         self.n_of_points = len(data)
 
@@ -17,8 +19,10 @@ class TSM:
         self.crossover_rate = crossover_rate
         self.mating_pool_size = int(population_size * mating_pool_size_factor)
 
-        self.distances = [[-1] * self.n_of_points for _ in range(self.n_of_points)]
+        self.distances = [
+            [-1] * self.n_of_points for _ in range(self.n_of_points)]
         self.__calculate_distances__()
+        self.__calculate_optimality_metric()
         self.population = []
         self.fitness = []
 
@@ -26,15 +30,56 @@ class TSM:
         return self.data
 
     def __calculate_distance__(self, point1, point2):
-        distance = math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+        distance = math.sqrt((point1[0] - point2[0])
+                             ** 2 + (point1[1] - point2[1]) ** 2)
         return distance
 
     def __calculate_distances__(self):
         for i in range(self.n_of_points):
             for j in range(i + 1, self.n_of_points):
-                distance = self.__calculate_distance__(self.data[i], self.data[j])
+                distance = self.__calculate_distance__(
+                    self.data[i], self.data[j])
                 self.distances[i][j] = distance
                 self.distances[j][i] = distance
+
+    def __calculate_optimality_metric(self):
+        # Minimum spanning tree (MST) + 1-tree heuristic to find the heighest 1-tree MST
+        lower_bound = 0
+
+        for exclude in range(self.n_of_points):
+            curr_bound = 0
+            unvisited = set(range(self.n_of_points))
+            unvisited.remove(exclude)
+            # Start from a random point
+            start_point = self.__get_random_point(unvisited)
+            unvisited.remove(start_point)
+
+            distances = {(start_point, j)
+                          : self.distances[start_point][j] for j in unvisited}
+            # Prim's algorithm to find the MST
+            while unvisited:
+                min_key = min(distances, key=distances.get)
+                curr_bound += distances[min_key]
+                new_visited = min_key[1]
+                unvisited.remove(new_visited)
+                # Update distances for the new visited point
+                {distances.pop(key) for key in list(
+                    distances.keys()) if key[1] == new_visited}
+                distances.update(
+                    {(new_visited, j): self.distances[new_visited][j] for j in unvisited})
+            # add 1-tree from the point we excluded
+            distances = [self.distances[exclude][j]
+                         for j in range(self.n_of_points) if j != exclude]
+            two_smallest = sorted(distances)[:2]
+            curr_bound += sum(two_smallest)
+
+            if curr_bound > lower_bound:
+                lower_bound = curr_bound
+
+        self.optimality_metric = lower_bound
+
+    def __get_random_point(self, unvisited):
+        return random.choice(list(unvisited))
 
     def __calculate_fitness__(self, solution):
         total_distance = 0
@@ -42,11 +87,13 @@ class TSM:
             total_distance += self.distances[solution[i]][solution[i + 1]]
         total_distance += self.distances[solution[-1]][solution[0]]
         return total_distance
-    
+
     def __calculate_population_fitness__(self):
-        self.fitness = [self.__calculate_fitness__(solution) for solution in self.population]
-        self.best_solution = min(self.population, key=self.__calculate_fitness__)
-    
+        self.fitness = [self.__calculate_fitness__(
+            solution) for solution in self.population]
+        self.best_solution = min(
+            self.population, key=self.__calculate_fitness__)
+
     def __initialize_population__(self):
         self.population = []
         for _ in range(self.population_size):
@@ -60,22 +107,23 @@ class TSM:
         probabilities = [f / total_fitness for f in self.fitness]
         parents = random.choices(self.population, weights=probabilities, k=2)
         return parents
-    
+
     def __select_parents_from_mating_pool__(self):
         self.__sort_population__()
         mating_pool = self.population[:self.mating_pool_size]
         total_fitness = sum(self.fitness[:self.mating_pool_size])
-        probabilities = [f / total_fitness for f in self.fitness[:self.mating_pool_size]]
+        probabilities = [
+            f / total_fitness for f in self.fitness[:self.mating_pool_size]]
         parents = random.choices(mating_pool, weights=probabilities, k=2)
         return parents
-    
+
     def __mutate__(self, individual):
         for i in range(self.n_of_points):
             if random.random() < self.mutation_rate:
                 j = random.randint(0, self.n_of_points - 1)
                 individual[i], individual[j] = individual[j], individual[i]
         return individual
-    
+
     def __crossover__(self):
         parent1, parent2 = self.__select_parents__()
 
@@ -83,21 +131,23 @@ class TSM:
         point2 = random.randint(point1 + 1, self.n_of_points)
         child = [-1] * self.n_of_points
         child[point1:point2] = parent1[point1:point2]
-        
+
         p2_index = 0
         for i in range(self.n_of_points):
             if child[i] == -1:
                 while parent2[p2_index] in child:
                     p2_index += 1
                 child[i] = parent2[p2_index]
-        
+
         child = self.__mutate__(child)
         return child
-    
+
     def __sort_population__(self):
-        self.population = sorted(self.population, key=self.__calculate_fitness__)
+        self.population = sorted(
+            self.population, key=self.__calculate_fitness__)
 
     def run(self, print_progress=False):
+        # Output: best solution, cost of the best solution, optimality metric * cost
         random.seed(self.seed)
         self.__initialize_population__()
 
@@ -107,7 +157,8 @@ class TSM:
             self.__sort_population__()
 
             if print_progress:
-                print("Best solution so far:", self.best_solution, "with cost:", self.__calculate_fitness__(self.best_solution))
+                print("Best solution so far:", self.best_solution,
+                      "with cost:", self.__calculate_fitness__(self.best_solution))
 
             new_population.extend(self.population[:self.elite_size])
 
@@ -121,6 +172,8 @@ class TSM:
 
             self.population = new_population
 
-
         self.__calculate_population_fitness__()
-        return self.best_solution, self.__calculate_fitness__(self.best_solution)
+        best_sol_fitness = self.__calculate_fitness__(self.best_solution)
+        opt_gap = (best_sol_fitness - self.optimality_metric) / \
+            self.optimality_metric
+        return self.best_solution, best_sol_fitness, opt_gap
--- main.py
+++ main.py
@@ -5,9 +5,15 @@ if __name__ == "__main__":
         data = f.read()
 
     data = data.split('\n')
-    data = [line.split()[1:] for line in data if line.strip()] 
+    data = [line.split()[1:] for line in data if line.strip()]
     data = [[float(num) for num in line] for line in data]
 
     travelling_salesman = TSM(data)
-    best_path, cost = travelling_salesman.run()
-    print("Best path:", best_path, "with cost:", cost)
+    best_path, cost, optimality_gap = travelling_salesman.run()
+    print(f"--- TSP Solution Details ---")
+    print(f"Path Discovered: {best_path}")
+    print(f"Calculated Tour Cost: {cost:.2f}")
+    print(
+        f"Relative to the lower bound, this solution is {optimality_gap * 100:.2f}% from optimal.")
+    print(f"NOTE:: Our calculated 'optimal' serves as a measure against the 1-tree relaxation, not necessarily the exact optimal solution.")
+    print(f"--------------------------")
